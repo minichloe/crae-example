c3ef945893fb0f7a0443ae618c4af1cfcf743554
Chloe Chong <chloechong.us@gmail.com>
2018-06-28 02:11:12 -0400

    Configured static files

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 // Create the server
 const app = express()
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
   try {
@@ -24,6 +27,11 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
   } catch (err) {
     next(err)
   }
+})
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 // Choose the port and start the server
